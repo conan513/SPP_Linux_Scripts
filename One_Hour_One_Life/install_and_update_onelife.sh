0b4d643f3d4b01ec1c783537485da8848f07595b
ConanHUN <conanhun@gmail.com>
2019-05-08 21:44:40 +0200

    One Hour One Life game and server builder script

---
+++ One_Hour_One_Life/install_and_update_onelife.sh
@@ -0,0 +1,37 @@
+echo
+echo Updating minorGems...
+echo
+git clone https://github.com/jasonrohrer/minorGems.git
+cd ./minorGems
+git pull
+cd  ..
+echo
+echo Updating OneLife
+echo
+git clone https://github.com/jasonrohrer/OneLife.git
+cd ./OneLife
+git pull
+cd ..
+echo
+echo Updating OneLifeData7
+echo
+git clone https://github.com/jasonrohrer/OneLifeData7.git
+cd ./OneLifeData7
+git pull
+cd ..
+echo
+echo Starting building the game...
+echo
+cd ./OneLife
+chmod u+x ./configure
+./configure 1
+cd ./build/source
+./makeLinuxBuildFromSourceBundle SPP
+cd ../..
+echo
+echo Starting building the server...
+echo
+cd ./server
+chmod u+x ./configure
+./configure 1
+make
