9a5544b98d0156a3da3230b9b291e0f945dc1b0e
ConanHUN <conanhun@gmail.com>
2019-05-26 05:28:12 +0200

    Teamspeak3 server installer added

---
+++ Teamspeak_Server/teamspeak3_server_x86.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+file="./teamspeak3-server_linux_x86/ts3server_minimal_runscript.sh"
+if [ ! -f "$file" ]
+then
+    echo "Teamspeak server not found, installing..."
+    echo
+    wget -N https://files.teamspeak-services.com/releases/server/3.7.1/teamspeak3-server_linux_x86-3.7.1.tar.bz2
+    tar -jxvf ./teamspeak3-server_linux_x86-3.7.1.tar.bz2
+fi
+ 
+cd ./teamspeak3-server_linux_x86
+./ts3server_minimal_runscript.sh license_accepted=1
